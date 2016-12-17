16c2ecfd93f637053b5af3eaf1b6876e31d02ddb
Ryan Burge <ryanburge@gmail.com>
2016-12-17 15:55:04 -0600

    Finished the recodes.

--- recoding_everything.R
+++ recoding_everything.R
@@ -150,9 +150,121 @@ abc$djtshare <- abc$q22_2
 abc$hrcshare[abc$hrcshare == 0] <- NA
 abc$djtshare[abc$djtshare == 0] <- NA
 
+## Reason for Vote
+abc$votereason <- abc$q67
+abc$votereason[abc$votereason==1] <- "Abortion"
+abc$votereason[abc$votereason==2] <- "Immigration"
+abc$votereason[abc$votereason==3] <- "Judicial Appointments"
+abc$votereason[abc$votereason==4] <- "Religious Freedom"
+abc$votereason[abc$votereason==5] <- "Hillary Clinton"
+abc$votereason[abc$votereason==6] <- "Donald Trump"
+abc$votereason[abc$votereason==7] <- "Treatment of Women"
+abc$votereason[abc$votereason==8] <- "Temperament"
+abc$votereason[abc$votereason==9] <- "Something Else"
 
+## Frequency of Election Discussion
 
+abc$discuss <- abc$q63
+abc$peoplenumber <- abc$q64_1
 
+## Election Questions
+abc$witness <- 8 - abc$q66_1
+abc$witness <- recode(abc$witness, "8=0")
+abc$agenda <- 8 - abc$q66_2
+abc$agenda <- recode(abc$agenda, "8=0")
+abc$doubts <- 8 - abc$q66_7
+abc$doubts <- recode(abc$doubts, "8=0")
+abc$disagree <- 8 - abc$q66_3
+abc$disagree <- recode(abc$disagree, "8=0")
+abc$rightdead <- 8 - abc$q66_4
+abc$rightdead <- recode(abc$rightdead, "8=0")
+abc$bond <- 8 - abc$q66_5
+abc$bond <- recode(abc$bond, "8=0")
+abc$immoral <- 8 - abc$q66_6
+abc$immoral <- recode(abc$immoral, "8=0")
 
+## Attendance Numbers
+abc$members <- abc$q28_1_text
+abc$attendance <- abc$q28_2_text
+abc$active <- abc$q28_3_text
 
+## Demographics of Congregation
 
+abc$size <- abc$q29
+abc$class <- abc$q30
+
+## Compare Yourself to Congreation
+abc$compare <- (abc$q32_1 + abc$q32_2 + abc$q32_3 + abc$q32_4 + abc$q32_5)/5
+ggplot(abc %>% filter(compare >0), aes(compare)) + geom_bar()
+
+## Compare Yourself to Community
+abc$comparecomm <- abc$q26
+
+## Compare Congregation to the Community 
+abc$comparecong <- (abc$q60_1 + abc$q60_2 + abc$q60_3 + abc$q60_4 + abc$q60_5)/5
+
+## Gender 
+abc$male <- recode(abc$q38, "1=1; else=0")
+
+## Political Affiliation
+
+abc$repubid <- recode(abc$q39, "8=0")
+abc$conideo <- abc$q68
+
+# Race
+abc$white <- abc$q41_1
+abc$black <- recode(abc$q41_2, "1=2")
+abc$hispanic <- recode(abc$q41_3, "1=3")
+abc$native <- recode(abc$q41_6, "1=4")
+abc$race <- abc$white + abc$black + abc$hispanic + abc$native
+abc$race[abc$race==1] <- "White"
+abc$race[abc$race==2] <- "Black"
+abc$race[abc$race==3] <- "Hispanic"
+abc$race[abc$race==4] <- "Native American"
+abc$race[abc$race==5] <- NA
+
+## Religious Labels 
+
+count <- read.csv("count.csv")
+ggplot(count, aes(x=label, y = count)) + geom_col()
+
+## Inclusive Sermon Messages
+
+abc$include1 <- 6- abc$q24_1
+abc$include1 <- recode(abc$include1, "6=0")
+abc$include2 <- 6- abc$q24_2
+abc$include2 <- recode(abc$include2, "6=0")
+abc$include3 <- abc$q24_3
+abc$include4 <- 6- abc$q24_4
+abc$include4 <- recode(abc$include4, "6=0")
+abc$include <- (abc$include1 + abc$include2 + abc$include3 + abc$include4)/20
+
+## Religious Conservative
+
+abc$relcon1 <- 6- abc$q44_1
+abc$relcon2 <- 6- abc$q44_2
+abc$relcon3 <- 6- abc$q44_3
+abc$relcon4 <- 6- abc$q44_4
+abc$relcon5 <- 6- abc$q44_5
+abc$relcon6 <- 6- abc$q44_6
+abc$relcon1 <- recode(abc$relcon1, "6=0")
+abc$relcon2 <- recode(abc$relcon2, "6=0")
+abc$relcon3 <- recode(abc$relcon3, "6=0")
+abc$relcon4 <- recode(abc$relcon4, "6=0")
+abc$relcon5 <- recode(abc$relcon5, "6=0")
+abc$relcon6 <- recode(abc$relcon6, "6=0")
+
+abc$relcon <- abc$relcon1 + abc$relcon2 + abc$relcon3 + abc$relcon4 + abc$relcon5 + abc$relcon6
+abc$relcon <- abc$relcon/30
+
+## Years of Service
+abc <- read.csv("abc.csv")
+abc$yrsmin <- abc$q45_1_text
+abc$yrscong <- abc$q45_2_text
+
+## Age
+abc$age<- 2016 - abc$q46_1
+
+## Education
+
+abc$educ <- abc$q48
