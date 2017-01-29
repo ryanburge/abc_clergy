782526c748967b1ae070b1f56556517f9856e2bf
Ryan Burge <ryanburge@gmail.com>
2017-01-28 21:24:31 -0600

    label changes.

--- waffle.R
+++ waffle.R
@@ -2,6 +2,6 @@
 library(waffle)
 library(extrafont)
 
-parts <- c(`Total Sampled`=2, `Total Contacted`=17, `Churches Not Contacted`=36)
+parts <- c(`Sampled`=2, `Contacted`=17, `Not Contacted`=36)
 waffle(parts, rows=5, size =3, xlab = "1 square = 100 churches", colors = c("#FF5A5F","#FFB400", "#007A87"))  + 
   theme(text=element_text(size=18, family="KerkisSans"))
