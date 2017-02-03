cdfdda335a66bb73c02d77ae9c9baad970e08198
Ryan Burge <ryanburge@gmail.com>
2017-02-03 10:18:38 -0600

    color

--- waffle.R
+++ waffle.R
@@ -3,5 +3,5 @@ library(waffle)
 library(extrafont)
 
 parts <- c(`Sampled`=2, `Contacted`=17, `Not Contacted`=36)
-waffle(parts, rows=5, size =3, xlab = "1 square = 100 churches", colors = c("#FF5A5F","#FFB400", "#007A87"))  + 
+waffle(parts, rows=5, size =1, xlab = "1 square = 100 churches", colors = c("#FF5A5F","#FFB400", "#007A87"))  + 
   theme(text=element_text(size=18, family="KerkisSans"))
