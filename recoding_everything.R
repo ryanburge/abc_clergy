8da612fbdc7fb3f77ae45a2ec4ed1dcc463865d7
Ryan Burge <ryanburge@gmail.com>
2017-03-03 10:39:06 -0600

    changing recode to Recode

--- recoding_everything.R
+++ recoding_everything.R
@@ -32,20 +32,20 @@ abc$study <- abc$q5_9 + abc$q10_9 + abc$q13_9
 abc$register <- abc$q5_10 + abc$q10_10 + abc$q13_10
 
 ##Outreach activities
-abc$out1 <- recode(abc$q6_1, "1=1; else=0")
-abc$out2 <- recode(abc$q6_2, "1=1; else=0")
-abc$out3 <- recode(abc$q6_3, "1=1; else=0")
-abc$out4 <- recode(abc$q6_4, "1=1; else=0")
+abc$out1 <- Recode(abc$q6_1, "1=1; else=0")
+abc$out2 <- Recode(abc$q6_2, "1=1; else=0")
+abc$out3 <- Recode(abc$q6_3, "1=1; else=0")
+abc$out4 <- Recode(abc$q6_4, "1=1; else=0")
 
-abc$out5 <- recode(abc$q11_1, "1=1; else=0")
-abc$out6 <- recode(abc$q11_2, "1=1; else=0")
-abc$out7 <- recode(abc$q11_3, "1=1; else=0")
-abc$out8 <- recode(abc$q11_4, "1=1; else=0")
+abc$out5 <- Recode(abc$q11_1, "1=1; else=0")
+abc$out6 <- Recode(abc$q11_2, "1=1; else=0")
+abc$out7 <- Recode(abc$q11_3, "1=1; else=0")
+abc$out8 <- Recode(abc$q11_4, "1=1; else=0")
 
-abc$out9 <- recode(abc$q14_1, "1=1; else=0")
-abc$out10 <- recode(abc$q14_2, "1=1; else=0")
-abc$out11 <- recode(abc$q14_3, "1=1; else=0")
-abc$out12 <- recode(abc$q14_4, "1=1; else=0")
+abc$out9 <- Recode(abc$q14_1, "1=1; else=0")
+abc$out10 <- Recode(abc$q14_2, "1=1; else=0")
+abc$out11 <- Recode(abc$q14_3, "1=1; else=0")
+abc$out12 <- Recode(abc$q14_4, "1=1; else=0")
 
 abc$outevent <- abc$out1 + abc$out5 + abc$out9 
 abc$outworship <- abc$out2 + abc$out6 + abc$out10
@@ -56,26 +56,26 @@ abc$out <- abc$out1 + abc$out2 + abc$out3 + abc$out4 + abc$out5 + abc$out6 + abc
 
 ## Make unpopular political statement
 
-abc$state1 <- recode(abc$q78_1, "1=1; else=0")
-abc$state2 <- recode(abc$q78_2, "1=1; else=0")
-abc$state3 <- recode(abc$q78_3, "1=1; else=0")
-abc$state4 <- recode(abc$q78_4, "1=1; else=0")
-abc$state5 <- recode(abc$q78_5, "1=1; else=0")
-abc$state6 <- recode(abc$q78_6, "1=1; else=0")
+abc$state1 <- Recode(abc$q78_1, "1=1; else=0")
+abc$state2 <- Recode(abc$q78_2, "1=1; else=0")
+abc$state3 <- Recode(abc$q78_3, "1=1; else=0")
+abc$state4 <- Recode(abc$q78_4, "1=1; else=0")
+abc$state5 <- Recode(abc$q78_5, "1=1; else=0")
+abc$state6 <- Recode(abc$q78_6, "1=1; else=0")
 
-abc$state7 <- recode(abc$q77_1, "1=1; else=0")
-abc$state8 <- recode(abc$q77_2, "1=1; else=0")
-abc$state9 <- recode(abc$q77_3, "1=1; else=0")
-abc$state10 <- recode(abc$q77_4, "1=1; else=0")
-abc$state11 <- recode(abc$q77_5, "1=1; else=0")
-abc$state12 <- recode(abc$q77_6, "1=1; else=0")
+abc$state7 <- Recode(abc$q77_1, "1=1; else=0")
+abc$state8 <- Recode(abc$q77_2, "1=1; else=0")
+abc$state9 <- Recode(abc$q77_3, "1=1; else=0")
+abc$state10 <- Recode(abc$q77_4, "1=1; else=0")
+abc$state11 <- Recode(abc$q77_5, "1=1; else=0")
+abc$state12 <- Recode(abc$q77_6, "1=1; else=0")
 
-abc$state13 <- recode(abc$q25_1, "1=1; else=0")
-abc$state14 <- recode(abc$q25_2, "1=1; else=0")
-abc$state15 <- recode(abc$q25_3, "1=1; else=0")
-abc$state16 <- recode(abc$q25_4, "1=1; else=0")
-abc$state17 <- recode(abc$q25_5, "1=1; else=0")
-abc$state18 <- recode(abc$q25_6, "1=1; else=0")
+abc$state13 <- Recode(abc$q25_1, "1=1; else=0")
+abc$state14 <- Recode(abc$q25_2, "1=1; else=0")
+abc$state15 <- Recode(abc$q25_3, "1=1; else=0")
+abc$state16 <- Recode(abc$q25_4, "1=1; else=0")
+abc$state17 <- Recode(abc$q25_5, "1=1; else=0")
+abc$state18 <- Recode(abc$q25_6, "1=1; else=0")
 
 abc$sign <- abc$state1 + abc$state7 + abc$state13
 abc$bumper <- abc$state2 + abc$state8 + abc$state14
@@ -91,40 +91,40 @@ abc$state <- abc$state1 + abc$state2 + abc$state3 + abc$state4 + abc$state5 + ab
 abc$involve1 <- 8 - abc$q4_1
 abc$involve2 <- 8 - abc$q4_2
 abc$involve3 <- 8 - abc$q4_3
-abc$involve1 <- recode(abc$involve1, "8=0")
-abc$involve2 <- recode(abc$involve2, "8=0")
-abc$involve3 <- recode(abc$involve3, "8=0")
+abc$involve1 <- Recode(abc$involve1, "8=0")
+abc$involve2 <- Recode(abc$involve2, "8=0")
+abc$involve3 <- Recode(abc$involve3, "8=0")
 
 abc$involve4 <- 8 - abc$q9_1
 abc$involve5 <- 8 - abc$q9_2
 abc$involve6 <- 8 - abc$q9_3
-abc$involve4 <- recode(abc$involve4, "8=0")
-abc$involve5 <- recode(abc$involve5, "8=0")
-abc$involve6 <- recode(abc$involve6, "8=0")
+abc$involve4 <- Recode(abc$involve4, "8=0")
+abc$involve5 <- Recode(abc$involve5, "8=0")
+abc$involve6 <- Recode(abc$involve6, "8=0")
 
 abc$involve7 <- 8 - abc$q12_1
 abc$involve8 <- 8 - abc$q12_2
 abc$involve9 <- 8 - abc$q12_3
-abc$involve7 <- recode(abc$involve7, "8=0")
-abc$involve8 <- recode(abc$involve8, "8=0")
-abc$involve9 <- recode(abc$involve9, "8=0")
+abc$involve7 <- Recode(abc$involve7, "8=0")
+abc$involve8 <- Recode(abc$involve8, "8=0")
+abc$involve9 <- Recode(abc$involve9, "8=0")
 
 abc$involve <- abc$involve1 + abc$involve2 + abc$involve3 + abc$involve4 + abc$involve5 + abc$involve6 + abc$involve7 + abc$involve8 + abc$involve9
 abc$involve <- abc$involve/21
 
 ## Right Direction
-abc$rightdirect <- recode(abc$q73, "1=7; 4=6; 5=5; 6=4; 7=3; 8=2; 9=1")
+abc$rightdirect <- Recode(abc$q73, "1=7; 4=6; 5=5; 6=4; 7=3; 8=2; 9=1")
 
 ## Trust in Govt
 abc$trust <- 8- abc$q74
 
 ## Clergy Political Representation
 
-abc$rep1 <- recode(abc$q15_1, "1=1; else=0")
-abc$rep2 <- recode(abc$q15_2, "1=1; else=0")
-abc$rep3 <- recode(abc$q15_3, "1=1; else=0")
-abc$rep4 <- recode(abc$q15_4, "1=1; else=0")
-abc$rep5 <- recode(abc$q15_5, "1=1; else=0")
+abc$rep1 <- Recode(abc$q15_1, "1=1; else=0")
+abc$rep2 <- Recode(abc$q15_2, "1=1; else=0")
+abc$rep3 <- Recode(abc$q15_3, "1=1; else=0")
+abc$rep4 <- Recode(abc$q15_4, "1=1; else=0")
+abc$rep5 <- Recode(abc$q15_5, "1=1; else=0")
 
 abc$rep <- abc$rep1 + abc$rep2 + abc$rep3 + abc$rep4 + abc$rep5
 
@@ -135,11 +135,11 @@ abc$rav2 <- 6- abc$q18_3
 abc$rav3 <- 6- abc$q18_5
 abc$rav4 <- 6- abc$q18_7
 abc$rav5 <- 6- abc$q18_9
-abc$rav1 <- recode(abc$rav1, "6=0")
-abc$rav2 <- recode(abc$rav2, "6=0")
-abc$rav3 <- recode(abc$rav3, "6=0")
-abc$rav4 <- recode(abc$rav4, "6=0")
-abc$rav5 <- recode(abc$rav5, "6=0")
+abc$rav1 <- Recode(abc$rav1, "6=0")
+abc$rav2 <- Recode(abc$rav2, "6=0")
+abc$rav3 <- Recode(abc$rav3, "6=0")
+abc$rav4 <- Recode(abc$rav4, "6=0")
+abc$rav5 <- Recode(abc$rav5, "6=0")
 abc$rav <- abc$rav1 + abc$rav2 + abc$rav3 + abc$rav4 + abc$rav5
 abc$rav <- abc$rav/5
 
@@ -150,11 +150,11 @@ abc$dem2 <- 6 - abc$q20_2
 abc$dem3 <- 6 - abc$q20_3
 abc$dem4 <- 6 - abc$q20_4
 abc$dem5 <- 6 - abc$q20_5
-abc$dem1 <- recode(abc$dem1, "6=0")
-abc$dem2 <- recode(abc$dem2, "6=0")
-abc$dem3 <- recode(abc$dem3, "6=0")
-abc$dem4 <- recode(abc$dem4, "6=0")
-abc$dem5 <- recode(abc$dem5, "6=0")
+abc$dem1 <- Recode(abc$dem1, "6=0")
+abc$dem2 <- Recode(abc$dem2, "6=0")
+abc$dem3 <- Recode(abc$dem3, "6=0")
+abc$dem4 <- Recode(abc$dem4, "6=0")
+abc$dem5 <- Recode(abc$dem5, "6=0")
 
 abc$dem <- abc$dem1 + abc$dem2 + abc$dem3 + abc$dem4 + abc$dem5
 
@@ -195,19 +195,19 @@ abc$peoplenumber <- abc$q64_1
 
 ## Election Questions
 abc$witness <- 8 - abc$q66_1
-abc$witness <- recode(abc$witness, "8=0")
+abc$witness <- Recode(abc$witness, "8=0")
 abc$agenda <- 8 - abc$q66_2
-abc$agenda <- recode(abc$agenda, "8=0")
+abc$agenda <- Recode(abc$agenda, "8=0")
 abc$doubts <- 8 - abc$q66_7
-abc$doubts <- recode(abc$doubts, "8=0")
+abc$doubts <- Recode(abc$doubts, "8=0")
 abc$disagree <- 8 - abc$q66_3
-abc$disagree <- recode(abc$disagree, "8=0")
+abc$disagree <- Recode(abc$disagree, "8=0")
 abc$rightdead <- 8 - abc$q66_4
-abc$rightdead <- recode(abc$rightdead, "8=0")
+abc$rightdead <- Recode(abc$rightdead, "8=0")
 abc$bond <- 8 - abc$q66_5
-abc$bond <- recode(abc$bond, "8=0")
+abc$bond <- Recode(abc$bond, "8=0")
 abc$immoral <- 8 - abc$q66_6
-abc$immoral <- recode(abc$immoral, "8=0")
+abc$immoral <- Recode(abc$immoral, "8=0")
 
 ## Attendance Numbers
 abc$members <- abc$q28_1_text
@@ -230,18 +230,18 @@ abc$comparecomm <- abc$q26
 abc$comparecong <- (abc$q60_1 + abc$q60_2 + abc$q60_3 + abc$q60_4 + abc$q60_5)/5
 
 ## Gender 
-abc$male <- recode(abc$q38, "1=1; else=0")
+abc$male <- Recode(abc$q38, "1=1; else=0")
 
 ## Political Affiliation
 
-abc$repubid <- recode(abc$q39, "8=0")
+abc$repubid <- Recode(abc$q39, "8=0")
 abc$conideo <- abc$q68
 
 # Race
 abc$white <- abc$q41_1
-abc$black <- recode(abc$q41_2, "1=2")
-abc$hispanic <- recode(abc$q41_3, "1=3")
-abc$native <- recode(abc$q41_6, "1=4")
+abc$black <- Recode(abc$q41_2, "1=2")
+abc$hispanic <- Recode(abc$q41_3, "1=3")
+abc$native <- Recode(abc$q41_6, "1=4")
 abc$race <- abc$white + abc$black + abc$hispanic + abc$native
 abc$race[abc$race==1] <- "White"
 abc$race[abc$race==2] <- "Black"
@@ -257,12 +257,12 @@ ggplot(count, aes(x=label, y = count)) + geom_col()
 ## Inclusive Sermon Messages
 
 abc$include1 <- 6- abc$q24_1
-abc$include1 <- recode(abc$include1, "6=0")
+abc$include1 <- Recode(abc$include1, "6=0")
 abc$include2 <- 6- abc$q24_2
-abc$include2 <- recode(abc$include2, "6=0")
+abc$include2 <- Recode(abc$include2, "6=0")
 abc$include3 <- abc$q24_3
 abc$include4 <- 6- abc$q24_4
-abc$include4 <- recode(abc$include4, "6=0")
+abc$include4 <- Recode(abc$include4, "6=0")
 abc$include <- (abc$include1 + abc$include2 + abc$include3 + abc$include4)/20
 
 ## Religious Conservative
@@ -273,12 +273,12 @@ abc$relcon3 <- 6- abc$q44_3
 abc$relcon4 <- 6- abc$q44_4
 abc$relcon5 <- 6- abc$q44_5
 abc$relcon6 <- 6- abc$q44_6
-abc$relcon1 <- recode(abc$relcon1, "6=0")
-abc$relcon2 <- recode(abc$relcon2, "6=0")
-abc$relcon3 <- recode(abc$relcon3, "6=0")
-abc$relcon4 <- recode(abc$relcon4, "6=0")
-abc$relcon5 <- recode(abc$relcon5, "6=0")
-abc$relcon6 <- recode(abc$relcon6, "6=0")
+abc$relcon1 <- Recode(abc$relcon1, "6=0")
+abc$relcon2 <- Recode(abc$relcon2, "6=0")
+abc$relcon3 <- Recode(abc$relcon3, "6=0")
+abc$relcon4 <- Recode(abc$relcon4, "6=0")
+abc$relcon5 <- Recode(abc$relcon5, "6=0")
+abc$relcon6 <- Recode(abc$relcon6, "6=0")
 
 abc$relcon <- abc$relcon1 + abc$relcon2 + abc$relcon3 + abc$relcon4 + abc$relcon5 + abc$relcon6
 abc$relcon <- abc$relcon/6
